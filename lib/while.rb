195e3e7fa1313d6d2fa1c127cf68aa6841fdfd8e
Carissa Sarreal <carissa.sarreal@gmail.com>
2020-09-13 04:04:11 +0000

    Automatically backed up by Learn

--- lib/while.rb
+++ lib/while.rb
@@ -1,10 +1,10 @@
-def using_while(phrase = "Wingardium Leviosa")
+def using_while
   levitation_force = 6
   
-  while levitation_force < 10
-  levitation_force += 1
-  puts "#{phrase}""
-end
+    while levitation_force < 10
+    levitation_force += 1
+    puts "Wingardium Leviosa
+  end
 end
 
 
